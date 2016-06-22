b28d78f0cccc2424e84bb8f65b5cd5b57e0fd3e1
Lasse S. Haslev <lasse@haslev.no>
2016-06-22 21:05:07 +0200

    Requires vue-modal instead

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
+module.exports = require( './components/vue-modal' );
+// import Vue from 'vue'
+// import App from './App.vue'
 
-new Vue({
-  el: 'body',
-  components: { App }
-})
+// new Vue({
+  // el: 'body',
+  // components: { App }
+// })
