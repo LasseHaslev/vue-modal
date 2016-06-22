1243565083b664c621f56f74f4f97bd834b0f02e
Lasse S. Haslev <lasse@haslev.no>
2016-06-22 09:24:28 +0200

    First commit

---
+++ src/vue-modal.js
@@ -0,0 +1,35 @@
+module.exports = {
+
+    template: require('./modal.template.html'),
+
+    data: function() {
+        return {
+            isOpen: false,
+        };
+    },
+
+    methods: {
+        open: function() {
+            this.isOpen = true;
+            // Disable scroll for body
+            document.body.classList.add('modal-open');
+
+            this.$dispatch( 'modal::open' );
+        },
+        close: function() {
+            this.isOpen = false;
+            // Enable scroll for body
+            document.body.classList.remove('modal-open');
+
+            this.$dispatch( 'modal::close' );
+        },
+        toggleOpen: function() {
+            this.isOpen = ! this.isOpen;
+            this.isOpen ? this.open() : this.close();
+        },
+    },
+
+    components: {
+        'remove-button': require( '../elements/remove-button' ),
+    }
+}
